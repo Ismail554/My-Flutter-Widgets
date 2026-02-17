9ce57cfae003051ab9d6cf0aab5dc3095ab7b177
Md Ismail Hosen <ismailshuvo554@gmail.com>
2026-02-17 23:03:09 +0600

    Notification Bell Icon

---
+++ notify_bell_toggle.dart
@@ -0,0 +1,24 @@
+ // // Notification Bell Icon
+          // GestureDetector(
+          //   onTap: onNotificationToggle,
+          //   child: Container(
+          //     width: 40.w,
+          //     height: 40.w,
+          //     decoration: BoxDecoration(
+          //       color: notificationsEnabled
+          //           ? AppColors.primaryColor.withOpacity(0.1)
+          //           : Colors.transparent,
+          //       borderRadius: BorderRadius.circular(12.r),
+          //     ),
+          //     child: Icon(
+          //       notificationsEnabled
+          //           ? Icons.notifications_active
+          //           : Icons.notifications_off,
+          //       color: notificationsEnabled
+          //           ? AppColors.primaryColor
+          //           : AppColors.grey,
+          //       size: 24.sp,
+          //     ),
+          //   ),
+          // ),
+import 'package:flutter/material.dart';
